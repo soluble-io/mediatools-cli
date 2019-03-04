d3dd6683c57b87592dc4de6140af68139eedce71
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-03-04 18:53:36 +0100

    Updated logger

--- src/Cli/Infra/StandardFileLoggerFactory.php
+++ src/Cli/Infra/StandardFileLoggerFactory.php
@@ -11,21 +11,42 @@ declare(strict_types=1);
 
 namespace Soluble\MediaTools\Cli\Infra;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use Soluble\MediaTools\Cli\Config\ConfigProvider;
 
 class StandardFileLoggerFactory
 {
+    public const STANDARD_LOG_FILE = 'mediatools.log';
+    public const ERROR_LOG_FILE    = 'mediatools-error.log';
+
     public function __invoke(ContainerInterface $container): LoggerInterface
     {
-        $logger = new \Monolog\Logger('soluble-mediatools-cli');
-        $logger->pushHandler(
-            new \Monolog\Handler\StreamHandler(
-                \Soluble\MediaTools\Cli\Config\ConfigProvider::getProjectLogDirectory() . DIRECTORY_SEPARATOR . 'mediatools.log',
-                \Monolog\Logger::INFO
-            )
-        );
+        $logger = new Logger('soluble-mediatools');
+        $logger->pushHandler(new StreamHandler(
+            self::getDefaultLogDirectory() . DIRECTORY_SEPARATOR . self::STANDARD_LOG_FILE,
+            Logger::INFO,
+            true,
+            0660
+        ));
+
+        $logger->pushHandler(new StreamHandler(
+            self::getDefaultLogDirectory() . DIRECTORY_SEPARATOR . self::ERROR_LOG_FILE,
+            Logger::ERROR,
+            true,
+            0660
+        ));
+
+        $logger->pushProcessor(new PsrLogMessageProcessor());
 
         return $logger;
+    }
+
+    public static function getDefaultLogDirectory(): string
+    {
+        return ConfigProvider::getProjectLogDirectory();
     }
 }
