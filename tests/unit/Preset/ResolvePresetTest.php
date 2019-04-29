695d5857687af626ac6709773966ebbf67b8dacd
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-04-29 18:24:18 +0200

    initial test

---
+++ tests/unit/Preset/ResolvePresetTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @see       https://github.com/soluble-io/soluble-mediatools-cli for the canonical repository
+ *
+ * @copyright Copyright (c) 2018-2019 Sébastien Vanvelthem. (https://github.com/belgattitude)
+ * @license   https://github.com/soluble-io/soluble-mediatools-cli/blob/master/LICENSE.md MIT
+ */
+
+namespace MediaToolsCliTest\Preset;
+
+use PHPUnit\Framework\TestCase;
+
+class ResolvePresetTest extends TestCase
+{
+    public function setUp(): void
+    {
+    }
+}
